fd2ba6b83249b673e435dd601a56f98c34aa99cb
Philipp Strube <pst@kubestack.com>
2020-07-25 12:22:19 +0200

    Add Istio test kustomization

--- main.tf
+++ main.tf
@@ -6,4 +6,6 @@ resource "kustomization_resource" "test" {
   for_each = data.kustomization.test.ids
 
   manifest = data.kustomization.test.manifests[each.value]
+
+  depends_on = [kustomization_resource.test["~G_v1_Namespace|~X|test-basic"]]
 }
